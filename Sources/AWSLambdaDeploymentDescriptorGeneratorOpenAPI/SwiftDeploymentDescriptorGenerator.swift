c24ca95f4e1021488d9cf16990f179d80c8573ec
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2024-06-17 11:30:01 +0200

    add a descriptive help to use the openAPI generator

--- Sources/AWSLambdaDeploymentDescriptorGeneratorOpenAPI/SwiftDeploymentDescriptorGenerator.swift
+++ Sources/AWSLambdaDeploymentDescriptorGeneratorOpenAPI/SwiftDeploymentDescriptorGenerator.swift
@@ -6,33 +6,12 @@ import Foundation
 struct DeploymentDescriptorGenerator {
     // TODO: this will become run when we include command line args library
     static func main() async throws {
-        let fm = FileManager.default
-        
-        let ddg = DeploymentDescriptorGenerator()
-        
-        // TODO: it should be possible to override with command line args
-        print("📖 Reading SAM JSON Schema")
-        let jsonSchemaPath = "Sources/AWSLambdaDeploymentDescriptorGeneratorSwiftSyntax/Resources/SAMJSONSchema.json"
-        guard fm.fileExists(atPath: jsonSchemaPath) else {
-            throw GeneratorError.fileNotFound(jsonSchemaPath)
-        }
-        let schema = try ddg.decode(file: jsonSchemaPath)
-        print("👉 \(jsonSchemaPath)")
-        
-        try await ddg.generate(from: schema)
-    }
-    
-    func decode(file: String) throws -> JSONSchema {
-        let schema = try Data(contentsOf: URL(filePath: file))
-        let decoder = JSONDecoder()
-        return try decoder.decode(JSONSchema.self, from: schema)
-    }
-    
-    func generate(from schema: JSONSchema) async throws {
+        print("""
+🛠️ Compile on the command line with:
 
-    }
-}
+swift run AWSLambdaDeploymentDescriptorGeneratorOpenAPI
 
-enum GeneratorError: Error {
-    case fileNotFound(String)
+Then, check the generated code in .build/plugins/outputs/...../Types.swift
+""")
+    }
 }
